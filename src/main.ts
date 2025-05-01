cfe80e6c76cbba4b71c64a593d331587d624c325
sumaya257 <sumayaece19@gmai.com>
2025-05-01 14:11:18 +0600

    updated-main-file

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: [
-      'https://cover-letter-generator-frontend-production.up.railway.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  });
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`🚀 Application is running on: http://localhost:${port}`);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
